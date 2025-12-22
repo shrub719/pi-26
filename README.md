c808abbc27d23f2466e9d422fa348e626a7bf877
shrub <shrub719@gmail.com>
2025-12-22 17:32:42 +0000

    add spigot formula

---
+++ README.md
@@ -0,0 +1,5 @@
+# a pi thing
+
+## Credits
+
+Spigot algorithm based on https://www.jjj.de/hfloat/spigot.haenel.txt
--- src/main.c
+++ src/main.c
@@ -1,7 +1,44 @@
 #include <stdio.h>
 
+#define BASE 26
+#define BASEL 26L
+#define N 1000
+#define LEN (26L * N) / 3 + 1
+
+unsigned j, predigit, nines, a[LEN], digit;
+long x, q, k, len, i;
+
+void output_digit(int d) {
+    printf("%c", 'A' + d);
+}
+
 int main() {
-    printf("no");
-    printf("yes");
+    int digit = 0;
+    for (j=N; j; ) {
+	    q = 0;
+	    k = LEN + LEN - 1;
+
+	    for (i = LEN; i; i--) {
+            x = (j == N ? 2*BASE : BASEL*a[i-1]) + q*i;
+            q = x / k;
+            a[i-1] = (unsigned)(x-q*k);
+            k -= 2;
+	    }
+
+	    k = x % BASE;
+	    if (k == BASE-1) ++nines;
+        else {
+            if (j) {
+                --j;
+                output_digit(predigit+x/BASE);
+            }
+
+            for (; nines; --nines) {
+                if (j) --j, printf(x >= BASE ? "A" : "Z");
+            }
+
+            predigit = (unsigned)k;
+	    }
+    }
     return 0;
 }
